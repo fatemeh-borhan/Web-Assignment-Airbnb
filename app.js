da40b560dd4d576d037d26559fb9769535421642
Fatemeh Borhan <fborhan-limoudehi@senecacollege.ca>
2019-10-10 00:02:00 -0400

    ok

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express= require("express");
 const exphbs  = require('express-handlebars');
 
 const app=express();
-//const PORT=process.env.PORT || 3000;
-const PORT=3000;
+const PORT=process.env.PORT || 3000;
+//const PORT=3000;
 
 app.use(express.static('public'))
 
