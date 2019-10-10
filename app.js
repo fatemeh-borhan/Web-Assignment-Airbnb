133a08e542635974757a74af2a14a4e1a9dd1e09
Fatemeh Borhan <fborhan-limoudehi@senecacollege.ca>
2019-10-09 21:33:46 -0400

    app.js

--- app.js
+++ app.js
@@ -21,9 +21,35 @@ app.get("/Registration",(req,res)=>{
 });
 
 app.get("/roomListing",(req,res)=>{
-    
-    
-    
+   //var rooms=[{title:"Lovely Room in Paris",
+            //   price:"$193 CAD/night",
+    //           img:"image/room1.jpeg},
+
+    //          {title:"Romantic Cabana with view",
+    //           price:"$234 CAD/night",
+//               img:"image/room2.jpeg"},
+
+//               {title:"The Joshua Tree House",
+    //            price:"$143 CAD/night",
+//                img:""image/room3.jpeg""},
+
+//               {title:"Unique Cobb Cottage - 2beds",
+    //            price:"$76 CAD/night",
+//                img:"image/room4.jpeg"},
+
+//               {title:"Stylish Downtown Cozy room",
+    //            price:"$98 CAD/night",
+//                img:"image/room5.jpeg"},
+
+//               {title:Private Studio Flat,
+    //            price:$167 CAD/night,
+//                img:"image/room6.jpeg"},
+//];
+//res.render("roomListing",{
+//Rooms:rooms
+//       });   
+//});
+  
     res.render("roomListing")   
     });
 
